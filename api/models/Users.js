6c1d766e0fb426486786ca3124d74b218454f1c7
puricamaykol <puricamaykol@gmail.com>
2017-10-20 12:05:44 -0300

    moving connection testing to index

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,12 +1,5 @@
 let db = require('./index.js');
-db.sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+
 
   const User = db.sequelize.define('MOCK_DATA', {
   id: {
